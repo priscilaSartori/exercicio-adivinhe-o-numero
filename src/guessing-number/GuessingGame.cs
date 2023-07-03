6518a53f8c53db144ce8158a1f92fd16940130f7
Priscila <priscilasartori2@gmail.com>
2023-07-03 18:57:26 -0300

    feat: AnalyzePlay

--- src/guessing-number/GuessingGame.cs
+++ src/guessing-number/GuessingGame.cs
@@ -67,7 +67,15 @@ public class GuessNumber
   //4 - Verifique a resposta da jogada
   public string AnalyzePlay()
   {
-    throw new NotImplementedException();
+    if (userValue > randomValue)
+      return "Tente um número MENOR";
+    else if (userValue < randomValue)
+      return "Tente um número MAIOR";
+    else
+    {
+      gameOver = true;
+      return "ACERTOU!";
+    }
   }
 
   //7 - Adicione uma opção para reiniciar o jogo
